b32d24734ef04657d144afdd9c17329eda7ee0e4
Stephen Demos <stphndemos@gmail.com>
2016-01-30 15:28:32 -0500

    added random field generation

--- src/MinesweeperREPA.hs
+++ src/MinesweeperREPA.hs
@@ -1,22 +1,25 @@
 module MinesweeperREPA where
 
+import Prelude hiding (intersperse, intercalate)
+
+import qualified Data.Vector as V
 import qualified Data.Array.Repa as R
-import           Data.Array.Repa.Stencil
-import           Data.Array.Repa.Algorithms.Randomish
+import qualified Data.Array.Repa.Repr.Vector as R
+import qualified Data.Array.Repa.Stencil as R
+import qualified Data.Array.Repa.Algorithms.Randomish as R
 
-intersperse :: a -> Vector a -> Vector a
-intersperse s v | null v    = v
-                | otherwise = head v `cons` prependToAll s (tail v)
+import System.Random
 
-prependToAll :: a -> Vector a -> Vector a
-prependToAll s v | null v    = v
-                 | otherwise = s `cons` (head v `cons` prependToAll s (tail v))
+intersperse :: a -> V.Vector a -> V.Vector a
+intersperse s v | V.null v    = v
+                | otherwise = V.head v `V.cons` prependToAll s (V.tail v)
 
-intercalate :: [a] -> Vector [a] -> [a]
-intercalate = (foldr1 (Prelude.++) .) . intersperse
+prependToAll :: a -> V.Vector a -> V.Vector a
+prependToAll s v | V.null v    = v
+                 | otherwise = s `V.cons` (V.head v `V.cons` prependToAll s (V.tail v))
 
-twodupdate :: Vector (Vector a) -> (Int,Int) -> a -> Vector (Vector a)
-twodupdate vs (x,y) a = vs // [( x, (vs!x) // [(y,a)] )]
+intercalate :: [a] -> V.Vector [a] -> [a]
+intercalate = (V.foldr1 (Prelude.++) .) . intersperse
 
 -- the danger rating of a minesweeper square
 -- I think that the Danger type is isomorphic to Maybe Integer
@@ -37,10 +40,9 @@ data Square = Square { getDanger  :: Danger
 -- the numbers are calculatable, but I think that we should calculate them 
 -- once at the beginning and then store them
 -- the field is row-major
-data Field = Field { getWidth  :: Int
-                   , getHeight :: Int
-                   , getField  :: R.Array R.U R.DIM2 Square
-                   }
+-- note: with the new repa representation, the width and height of the array
+-- are encoded in the array as the shape
+data Field = Field { getField :: R.Array R.V R.DIM2 Square }
 
 instance Show Danger where
     show Mine       = "*"
@@ -49,24 +51,33 @@ instance Show Danger where
     show (Danger d) = show d
 
 instance Show Square where
-    show (Square d True)  = show d
-    show (Square _ False) = " "
+    --show (Square d True)  = show d
+    --show (Square _ False) = " "
     --debug show
-    --show (Square d _) = show d
+    show (Square d _) = show d
 
 instance Show Field where
-    show = intercalate "\n" . map (intercalate " " . map show) . toUnboxed . getField
+    show (Field f) = let (R.Z R.:. x R.:. y) = R.extent f in 
+        V.foldl1' (++)
+          $ V.imap (\i a -> if i /= 0 then (if mod i y == 0 then '\n' else ' ') : show a else show a)
+          $ R.toVector f
+
+-- take n unique values from a list. the third argument is the seen list
+takeUnique :: (Eq n, Eq a, Num n) => n -> [a] -> [a]
+takeUnique = takeUnique' []
+takeUnique' :: (Eq n, Eq a, Num n) => [a] -> n -> [a] -> [a]
+takeUnique' seen 0 _      = seen
+takeUnique' seen n (x:xs) | x `notElem` seen = takeUnique' (x:seen) (n-1) xs
+                          | otherwise        = takeUnique' seen n xs
 
 -- generate a new field using one rng for x and one rng for y
 generateField :: (RandomGen g)
-              => g   -- random number generator for x
-              -> g   -- random number generator for y
+              => g   -- random number generator
               -> Int -- height of minefield
               -> Int -- width of minefield
               -> Int -- Number of mines
               -> Field
-generateField gx gy x y n = 
-    where rx = randomRs (0,x-1) gx
-          ry = randomRs (0,y-1) gy
-          ur = takeUnique n (zip rx ry) []
-          f  = Field x y
+generateField g x y n = Field f
+    where rs = takeUnique n $ randomRs (0, (x * y) - 1) g
+          fv = V.replicate (x * y) (Square (Danger 0) False) V.// zip rs (repeat $ Square Mine False)
+          f  = R.fromVector (R.Z R.:. x R.:. y) fv
