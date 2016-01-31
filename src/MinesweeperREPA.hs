486ea8851a2e5f8103245f4482ce3b3e3cba5eb3
Stephen Demos <stphndemos@gmail.com>
2016-01-31 16:15:43 -0500

    added danger updated convolution

--- src/MinesweeperREPA.hs
+++ src/MinesweeperREPA.hs
@@ -1,25 +1,17 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module MinesweeperREPA where
 
-import Prelude hiding (intersperse, intercalate)
+import Prelude hiding (map, zipWith)
 
 import qualified Data.Vector as V
-import qualified Data.Array.Repa as R
-import qualified Data.Array.Repa.Repr.Vector as R
-import qualified Data.Array.Repa.Stencil as R
-import qualified Data.Array.Repa.Algorithms.Randomish as R
+import           Data.Array.Repa as R hiding ((++))
+import           Data.Array.Repa.Repr.Vector as R
+import           Data.Array.Repa.Stencil as R
+import           Data.Array.Repa.Stencil.Dim2 as R
+import           Data.Array.Repa.Algorithms.Randomish as R
 
 import System.Random
-
-intersperse :: a -> V.Vector a -> V.Vector a
-intersperse s v | V.null v    = v
-                | otherwise = V.head v `V.cons` prependToAll s (V.tail v)
-
-prependToAll :: a -> V.Vector a -> V.Vector a
-prependToAll s v | V.null v    = v
-                 | otherwise = s `V.cons` (V.head v `V.cons` prependToAll s (V.tail v))
-
-intercalate :: [a] -> V.Vector [a] -> [a]
-intercalate = (V.foldr1 (Prelude.++) .) . intersperse
 
 -- the danger rating of a minesweeper square
 -- I think that the Danger type is isomorphic to Maybe Integer
@@ -33,6 +25,8 @@ data Danger = Mine | Danger Int deriving Eq
 data Square = Square { getDanger  :: Danger
                      , isRevealed :: Bool
                      } deriving Eq
+
+emptySquare = Square (Danger 0) False
 
 -- For minesweeper, there are a couple of things that we need to keep track of
 -- First, we need the locations of the mines
@@ -57,7 +51,7 @@ instance Show Square where
     show (Square d _) = show d
 
 instance Show Field where
-    show (Field f) = let (R.Z R.:. x R.:. y) = R.extent f in 
+    show (Field f) = let (R.Z R.:. x R.:. y) = R.extent f in
         V.foldl1' (++)
           $ V.imap (\i a -> if i /= 0 then (if mod i y == 0 then '\n' else ' ') : show a else show a)
           $ R.toVector f
@@ -77,7 +71,26 @@ generateField :: (RandomGen g)
               -> Int -- width of minefield
               -> Int -- Number of mines
               -> Field
-generateField g x y n = Field f
+generateField g x y n = updateDangers $ Field f
     where rs = takeUnique n $ randomRs (0, (x * y) - 1) g
-          fv = V.replicate (x * y) (Square (Danger 0) False) V.// zip rs (repeat $ Square Mine False)
+          fv = V.replicate (x * y) emptySquare V.// zip rs (repeat $ Square Mine False)
           f  = R.fromVector (R.Z R.:. x R.:. y) fv
+
+updateDangers :: Field -> Field
+updateDangers (Field f) = Field
+                        . computeVectorS -- convert from delayed array back to vector array
+                        . R.zipWith updateDanger f -- update the field with the new dangers we found
+                        . R.mapStencil2 (R.BoundConst 0) stencil -- perform the convolution
+                        . R.map (\s->if isMine s then 1 else 0) -- turn mines into 1s and others into 0s
+                        $ f
+    where stencil = [R.stencil2|1 1 1
+                                1 0 1
+                                1 1 1|]
+
+updateDanger :: Square -> Int -> Square
+updateDanger (Square (Danger _) r) d = Square (Danger d) r
+updateDanger s _ = s
+
+isMine :: Square -> Bool
+isMine (Square Mine _) = True
+isMine _               = False
